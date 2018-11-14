351497a00b5a879313c903314cad71f174c6cf63
Aaron Graves <agraves1@ucvts.tec.nj.us>
2018-11-14 14:46:33 -0500

    updooted

--- src/ATM.java
+++ src/ATM.java
@@ -1,31 +1,51 @@
 import java.util.Scanner;
 
 public class ATM {
-	static Scanner in = new Scanner(System.in);
+	private Scanner in;
 	private BankAccount account;
 	
-	public ATM(BankAccount account) {
+	ATM(BankAccount account) {
 		this.account = account;
 	}
 	
 	public static void main(String args[]) {
 		ATM atm = new ATM(new BankAccount(12, 12, 12, new AccountHolder(12, "Aaron", "908", "Address")));
-	    
+	    atm.run();
 	}
 	
-	public static BankAccount newAccount() {
-		int SSN; String name; String phone; String address;
-		System.out.println("Please enter your SSN: ");
-		SSN = in.nextInt();
-		in.nextLine();
-		System.out.println("Please enter your full name: ");
-		name = in.nextLine();
-		System.out.println("Enter your phone number: ");
-		phone = in.nextLine();
-		System.out.println("Enter your address: ");
-		address = in.nextLine();
+	public void run() {
+		in = new Scanner(System.in);
 		
-		AccountHolder user = new AccountHolder(SSN, name, phone, address);
+		// do all the stuff
+		// ask user for account #
+		System.out.println("Enter your account number: ");
+		int validAN = in.nextInt();
+		// ask user for pin
+		// validate account/pin
+		if (accountNumber == validAN) {
+			
+		}
+		// if valid, show menu
 		
+		
+		in.close();
 	}
+	
+//	public static BankAccount newAccount() {
+//		int SSN; String name; String phone; String address;
+//		System.out.println("Please enter your SSN: ");
+//		SSN = in.nextInt();
+//		in.nextLine();
+//		System.out.println("Please enter your full name: ");
+//		name = in.nextLine();
+//		System.out.println("Enter your phone number: ");
+//		phone = in.nextLine();
+//		System.out.println("Enter your address: ");
+//		address = in.nextLine();
+//		in.close();
+//		AccountHolder owner = new AccountHolder(SSN, name, phone, address);		
+//		
+//		return account;
+//		
+//	}
 }
